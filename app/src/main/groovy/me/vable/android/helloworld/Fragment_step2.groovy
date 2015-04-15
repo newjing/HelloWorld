5c421da03b0bb3b100e889b7da137ceb9d8d65a7
newjing <liu123jing@hotmail.com>
2015-04-15 10:54:50 +0800

    demo swissKnife in fragment

--- app/src/main/groovy/me/vable/android/helloworld/Fragment_step2.groovy
+++ app/src/main/groovy/me/vable/android/helloworld/Fragment_step2.groovy
@@ -15,20 +15,18 @@ public class Fragment_step2 extends Fragment{
     @InjectView(R.id.backBtn)
     Button nextBtn;
 
-    View view;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        Log.i("MainActivity--step2--onCreateView:","onCreateView")
-        view = inflater.inflate(R.layout.fragment_step2, container, false);
-        return view;
+         return inflater.inflate(R.layout.fragment_step2, container, false);
     }
 
     @Override
     void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState)
-        SwissKnife.inject(this, view);
+
+        //看笔记，只有在这里才能保证view已经生成出来了，之前的N个步骤函数里view都是空，所以SwissKnife也绑定不了
+        SwissKnife.inject(this, getView());
         SwissKnife.restoreState(this, savedInstanceState);
     }
 
@@ -41,7 +39,7 @@ public class Fragment_step2 extends Fragment{
             fm.popBackStack();
         } else {
             Log.i("MainActivity", "nothing on backstack, calling super");
-            //super.onBackPressed();  //看父类有没有定义什么返回响应
+            //super.onBackPressed();  //看父类or Activity什么的有没有定义什么返回响应需要调用
         }
 
     }
